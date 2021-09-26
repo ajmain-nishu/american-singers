e1cf03497abd7c2d9fcb83c6a142a808adc41162
Ajmain Nishu <ajmainnishu@gmail.com>
2021-09-26 09:31:10 +0600

    feat: Added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="text-center bg-info rounded py-4">
+            <h1>American Singers</h1>
+            <p>Music shouldn't be just a tune, it should be a touch</p>
+            <h2>Total Budget: 100 Million</h2>
+        </div>
+    );
+};
+
+export default Header;
